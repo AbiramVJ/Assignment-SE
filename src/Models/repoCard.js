6eb2da55cfc5dad499d7b60e8555dc76a0c7ddb2
abiram <abiram586@gmail.com>
2025-07-24 15:36:18 +0530

    model changes

--- src/Models/repoCard.js
+++ src/Models/repoCard.js
@@ -1,16 +1,16 @@
 export class RepoOwner {
   constructor(login, avatar_url) {
-    this.login = login;
-    this.avatar_url = avatar_url;
+    this.login = login ?? '';
+    this.avatar_url = avatar_url ?? '';
   }
 }
 
 export class Repo {
   constructor(id, name, description, stargazers_count, owner) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-    this.stargazers_count = stargazers_count;
-    this.owner = owner;
+    this.id = id ?? null;
+    this.name = name ?? '';
+    this.description = description ?? '';
+    this.stargazers_count = stargazers_count ?? 0;
+    this.owner = owner ?? new RepoOwner('', '');
   }
 }
